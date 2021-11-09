6dba53ebdda7c61a3866bcd207f7c597a0de2963
joaopedpaula <joao.paula@bandtec.com.br>
2021-11-09 11:33:41 -0300

    alterando arquivo de connection da api-sensor-dht11

--- api-sensor-dht11/app/connection.js
+++ api-sensor-dht11/app/connection.js
@@ -1,8 +1,7 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
 var connection = mysql.createConnection({
     host     : 'localhost',
-    port     : '3306',
     user     : 'root',
     password : 'password',
     database : 'acquatec'
